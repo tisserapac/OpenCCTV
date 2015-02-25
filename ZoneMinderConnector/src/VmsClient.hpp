b51b3eba028d25c302402280a1775e00f46127f5
Anjana Tissera <anjanat@gmail.com>
2015-02-25 08:52:22 +0700

    Addiing the common definition class files to the ZoneMinder connector

--- ZoneMinderConnector/src/VmsClient.hpp
+++ ZoneMinderConnector/src/VmsClient.hpp
@@ -8,13 +8,24 @@
 #ifndef VMSCLIENT_HPP_
 #define VMSCLIENT_HPP_
 
+#include <string>
+#include "ThreadSafeQueue.hpp"
+#include "Image.hpp"
+
+using namespace std;
+
 class VmsClient
 {
 public:
 	VmsClient(){}
 	virtual ~VmsClient(){}
+	virtual void produceImageObjects(ThreadSafeQueue<Image>* pQueue) = 0;
+	virtual bool init(string sParamString) = 0;
+	virtual bool isStillProducingImages() = 0;
 };
 
-
+// the type definitions for the class factories
+typedef VmsClient* create_t();
+typedef void destroy_t(VmsClient*);
 
 #endif /* VMSCLIENT_HPP_ */
